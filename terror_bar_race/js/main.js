1306783ea64e1c8789b9d5da2819b3c9656257b3
Arnav <arnavnegi14@gmail.com>
2023-04-10 21:49:30 +0530

    small change in csv

--- terror_bar_race/js/main.js
+++ terror_bar_race/js/main.js
@@ -1,8 +1,8 @@
 // Bar chart race for terrorism. Linear easing between keyframes.
-
 let data = [];
-d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/disease-burden-from-injuries.csv", function (d) {
+d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/annual-number-of-deaths-by-cause.csv", function (d) {
     d.Year = parseInt(d.Year);
+    d.Terrorism = parseInt(d.Terrorism)
     data.push(d);
 }).then(function () {
     console.log(data)
@@ -14,6 +14,10 @@ d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/
     const n = 12;
 
     const countryNames = new Set(data.map(d => d.Country));
-    
+    let dataMap = Array.from(d3.rollup(data,
+        ([d]) => d.Terrorism, d => duration.Year,  d => d.Country))
+        .sort(([a], [b]) => d3.ascending(a , b));
+
+    console.log(dataMap);
     
 });
