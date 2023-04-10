fa955ef0164617f14f6a79823d067f4777f8a5da
Arnav <arnavnegi14@gmail.com>
2023-04-10 09:14:26 +0530

    small change in csv

--- terror_bar_race/js/main.js
+++ terror_bar_race/js/main.js
@@ -1,1 +1,12 @@
-openDatabase = d3.csvParse("")
+// Bar chart race for terrorism. Linear easing between keyframes.
+
+let data = [];
+d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/disease-burden-from-injuries.csv", function (d) {
+    d.Year = parseInt(d.Year);
+    data.push(d);
+}).then(function () {
+    console.log(data)
+
+    const duration = 1000; // duration between keyframes
+    d3.group(data, d => d.Country)
+});
