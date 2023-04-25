c69b47fb2f044fdfd93c06c756c8184feeffc920
Arnav <arnavnegi14@gmail.com>
2023-04-26 02:37:51 +0530

    data changes

--- gdp-disease-scatterplot/js/main.js
+++ gdp-disease-scatterplot/js/main.js
@@ -1,10 +1,19 @@
 let ready = false;
 
+const startYear = 2000;
+const endYear = 2019;
+
 let dataArray = [];
 let healthExpenditure = [];
-d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/health_expenditure.csv", function(data) {
-        healthExpenditure.push(data);
-}).then(function() {
-    ready = true;
-    console.log(healthExpenditure);
+d3.csv("https://raw.githubusercontent.com/Arnav-Negi/data-viz-project/main/data/health_expenditure.csv", function (data) {
+    let toPush = {
+        "country": data["Country Name"]
+    }
+    for (let i = startYear; i <= endYear; i++) {
+        if (data[i.toString()] !== "")
+            toPush[i.toString()] = +data[i.toString()];
+    }
+    healthExpenditure.push(toPush);
+}).then(function () {
+    d3.csv()
 });
